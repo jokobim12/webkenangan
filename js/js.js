1b8224e37ca2bad96878b07a2adf93277a42aab2
jokobim12 <jokobimantaro88@gmail.com>
2025-03-16 23:08:23 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,28 @@
+const navSlide = () =>{
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+    burger.addEventListener("click",() => {
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toogle-burger")
+    });
+};
+
+navSlide();
+
+document.addEventListener("DOMContentLoaded", function () {
+    let path = window.location.pathname;
+    let page = path.split("/").pop().split(".")[0];
+
+    document.querySelectorAll("nav a").forEach(link => {
+        link.classList.remove("active");
+    });
+
+    if (page === "" || page === "index") {
+        document.getElementById("beranda").classList.add("active");
+    } else {
+        let activeLink = document.getElementById(page);
+        if (activeLink) {
+            activeLink.classList.add("active");
+        }
+    }
+});
