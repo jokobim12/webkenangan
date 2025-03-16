09f1e8de08f074b7e2b806ba60d1afe6f6302022
jokobim12 <jokobimantaro88@gmail.com>
2025-03-16 22:27:28 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
+// memberishkan
+window.onbeforeunload = ()=>{
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
+
 const navSlide = () =>{
     const burger = document.querySelector(".burger")
     const navLists = document.querySelector("nav")
